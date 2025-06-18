afbd20ff5c8cd0632fdd687ee8f906495d59c03d
Harsh Kumar <harshkumar@Harshs-MacBook-Pro.local>
2025-06-19 01:19:59 +0530

    added tableau images and updated views

--- Script.sql
+++ Script.sql
@@ -59,12 +59,47 @@ priority as (
 	select *
 	from filter_data
 	order by total_charges desc, monthly_charges desc, tenure desc
+	limit 10
 	
 )
 select * from priority;
 
+create or replace view top_contract as
+with clean_data as (
+	
+	select
+		tenure,
+		"MonthlyCharges" as monthly_charges,
+		"Contract" as contract_type,
+		"Churn" as churn ,
+		"TotalCharges" as total_charges
+	from churn_dataset
+	
+),
+filter_data as ( 
+	
+	select *
+	from clean_data
+	where churn = 'No' and total_charges is not null
+
+),
+priority as ( 
+	
+	select
+		AVG(tenure) as avg_tenure,
+		AVG(monthly_charges) as avg_month_charge,
+		AVG(total_charges) as avg_total_charge,
+		contract_type
+	from filter_data
+	group by contract_type
+	order by avg_total_charge desc, avg_month_charge desc, avg_tenure desc
+	
+)
+select * from priority;
+--drop view top_contract 
 select * from top_customers;
 
+--select count (distinct "customerID" ) as dist, count(*) as tot from churn_dataset;
 
 /*select * from monthly_churn
 limit 20;*/
